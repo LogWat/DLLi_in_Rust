f85fdcd439b91ac339d87d71836fa9a82276f306
LogWat <Nk8877wpt+Karan@gmail.com>
2021-09-21 23:34:58 +0900

    説明

--- dlli/src/main.rs
+++ dlli/src/main.rs
@@ -6,9 +6,10 @@ const WH_KEYBOARD_LL: i32 = 13;
 fn main() {
     unsafe {
         let hook_id = user32::SetWindowsHookExA(
-            WH_KEYBOAD_LL, 
-            lpfn: HOOKPROC, 
-            hmod: HINSTANCE, 
-            dwThreadId: DWORD)
+            WH_KEYBOARD_LL,                     // フックタイプ
+            lpfn: HOOKPROC,                     // windowがmessageを処理する際に呼び出す関数のaddr
+            hmod: HINSTANCE,                    // DLL
+            0                                   // 0: すべてのGUIスレッドにフックを設定
+        );
     }
 }
