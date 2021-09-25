c0d3798f819792b239cf69bc798b9a74a97118be
LogWat <Nk8877wpt+Karan@gmail.com>
2021-09-25 20:54:32 +0900

    fn api

--- dlli/src/main.rs
+++ dlli/src/main.rs
@@ -1,6 +1,8 @@
 extern crate user32;
 extern crate winapi;
 extern crate kernel32;
+extern crate w32_error;
+extern crate libc;
 
 use kernel32::{
     OpenProcess,
@@ -11,29 +13,35 @@ use winapi::{
         winnt::{
             PROCESS_CREATE_THREAD, // CreateRemoteThread
             PROCESS_VM_OPERATION,  // VirtualAllocEx
-            PROCESS_VM_WRITE       // WriteProcessMemory
+            PROCESS_VM_WRITE,      // WriteProcessMemory
         },
         errhandlingapi::{
             GetLastError
         }
     },
-    shared::minwindef::{
-        DWORD, 
-        FALSE,
-        BOOL
+    shared::{
+        minwindef::{
+            FALSE,
+            DWORD
+        },
+        /*ntdef::{
+            HANDLE
+        }*/
     }
 };
+use w32_error::W32Error;
+
+type HANDLE = *mut libc::c_void;
+
 
 fn main() {
     let word = input();
-    let pid: u32 = word.parse().unwrap();
-    println!("{}", pid);
+    let pid: DWORD = word.parse().unwrap();
 
     unsafe {
-        let process = Some(OpenProcess(
-            PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE, 
-            FALSE, 
-            pid)).unwrap_or_else(|| panic!("{}", GetLastError()));
+        let process = winapi_openprocess(pid).unwrap();
+
+        println!("{:?}", process);
 
         CloseHandle(process);
     }
@@ -43,4 +51,18 @@ fn input() -> String {
     let mut word = String::new();
     std::io::stdin().read_line(&mut word).ok();
     word.trim().to_string()
+}
+
+unsafe fn winapi_openprocess(pid: DWORD) -> Result<HANDLE, W32Error> {
+
+    let result = OpenProcess(
+        PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE, 
+        FALSE, 
+        pid);
+    
+    if result as usize == 0 {
+        Err(W32Error::new(0))
+    } else {
+        Ok(result)
+    }
 }
