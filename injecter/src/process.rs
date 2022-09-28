1e5dee053d49643cd9342aeea6d92d2f7b3fb2fc
LogWat <Nk8877wpt+Karan@gmail.com>
2022-09-28 17:05:35 +0900

    add: enum_process

--- injecter/src/process.rs
+++ injecter/src/process.rs
@@ -1,7 +1,12 @@
+use std::{mem, ptr};
 use winapi::{
     um::{
         winnt::{HANDLE, PROCESS_ALL_ACCESS},
-        processthreadsapi, handleapi,
+        processthreadsapi, handleapi, errhandlingapi,
+        tlhelp32,
+        tlhelp32::{
+            PROCESSENTRY32W, THREADENTRY32, TH32CS_SNAPPROCESS, TH32CS_SNAPTHREAD,
+        }
     },
 };
 
@@ -29,4 +34,38 @@ impl Process {
             pid,
         }
     }
+
+    pub fn is_valid(&self) -> bool {
+        self.handle != std::ptr::null_mut()
+    }
+}
+
+pub fn enumerate_process() -> Result<Vec<Process>, u32> {
+    let mut prs: Vec<Process> = Vec::new();
+    let mut psetry: PROCESSENTRY32W = unsafe { mem::zeroed() };
+    psetry.dwSize = mem::size_of::<PROCESSENTRY32W>() as u32;
+
+    let snap = unsafe {
+        tlhelp32::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
+    };
+
+    if snap == ptr::null_mut() {
+        return Err(unsafe { errhandlingapi::GetLastError() });
+    }
+
+    let mut ret = unsafe {
+        tlhelp32::Process32FirstW(snap, &mut psetry)
+    };
+    while ret != 0 {
+        prs.push(Process::new(psetry.th32ProcessID));
+        ret = unsafe {
+            tlhelp32::Process32NextW(snap, &mut psetry)
+        };
+    }
+
+    unsafe {
+        handleapi::CloseHandle(snap);
+    }
+
+    Ok(prs)
 }
